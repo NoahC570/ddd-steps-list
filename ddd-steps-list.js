6573f732505ac4d3721d22153cfdc1fff4c8bec7
NoahC570 <noah215557@gmail.com>
2025-04-14 20:57:37 -0400

    morew chnages made still no html in yet

--- ddd-steps-list.js
+++ ddd-steps-list.js
@@ -38,6 +38,65 @@ export class DddStepsList extends DDDSuper(I18NMixin(LitElement)) {
       locales: ["ar", "es", "hi", "zh"],
     });
   }
-
+  static get properties() {
+    return {
+      ...super.properties,
+      header: { type: String },
+      index: { type: Number },
+      description: { type: String },
+    };
+  }
+  updated(changedProperties) {
+    if (changedProperties.has('title')){
+      this.indexcount();
+    }
+  }
   
+  indexcount() {
+    const items = this.querySelectorAll("ddd-steps-list-item");
+    items.forEach((element, index) => {
+      element.count=index+1
+      console.log(element.count)
+    })
+  }
+  
+  valdiateChildren() { 
+    const slot = this.shadowRoot.querySelector("#step-slot");
+    if (!slot){
+      console.error("Slot #step-slot not found in shadowRoot.");
+      return;
+    }
+    const assignedElements = slot.assignedElements({
+      flatten: true,
+    });
+    assignedElements.forEach((child) => {
+      if (child.tagName.toLowerCase() !== "ddd-steps-list-item") {
+        console.warn("Invalid Tag");
+        child.remove();
+      }
+    })
+  }
+  
+    // Lit render the HTML
+    render() {
+      return html`
+  <div class="wrapper">
+  <div class='description'>
+    <h3>${this.title}</h3>
+      ${this.description}
+      </div>
+        <div class="steps-items">
+          <slot id="step-slot"></slot>
+        </div>
+  </div>`;
+    }
+    /**
+     * haxProperties integration via file reference
+     */
+    static get haxProperties() {
+      return new URL(`./lib/${this.tag}.haxProperties.json`, import.meta.url)
+        .href;
+    }
+  }
+  globalThis.customElements.define(DddStepsList.tag, DddStepsList);
   }
